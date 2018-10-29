a24616919dc8ba60ec4a2e06c8c86bdc82c87540
yojhop <yojhop@126.com>
2018-10-29 19:34:08 +0800

    add interval timer

--- timer.js
+++ timer.js
@@ -11,3 +11,23 @@ function stepTimer(util,cb,minDiff=10000,level=10){
         setTimeout(()=>{stepTimer(util,cb,minDiff,level)},diff)
     }
 }
+function interval(func,timeout,cb,errCb){
+    const toString=Object.prototype.toString
+    if(toString.call(func)==='[object Promise]'){
+        func.then(res=>{
+            cb&&cb(res)
+        }).catch(err=>{
+            errCb&&errCb(err)
+        }).then(()=>{
+            setTimeout(()=>{
+                interval(func,timeout,cb)
+            },timeout)
+        })
+    }
+    else if(toString.call(func)==='[object Function]'){
+        cb&&cb(func())
+        setTimeout(()=>{
+            interval(func,timeout,cb)
+        },timeout)
+    }
+}

fa0924d1a814f633d3be241cbe2d5d599f8ea017
yojhop <yojhop@126.com>
2018-12-04 15:50:05 +0800

    finish interval call logic

--- intervalCall.js
+++ intervalCall.js
@@ -1,43 +1,117 @@
 let cacheCalls = []
 function intervalCall(cb, interval, ...args) {
-  for (let callItem of cacheCalls) {
-    // invoked previously
+  for (let i = 0; i < cacheCalls.length; i++) {
+    let callItem = cacheCalls[i]
     if (callItem.cb === cb) {
-      callItem.interval = interval
-      try {
-        clearTimeout(callItem.timeoutId)
-        console.log('clear Timeout', callItem.timeoutId)
-      } catch (e) {}
-      let curTime = new Date().getTime()
-      if (curTime - callItem.lastCall >= interval) {
-        startCallLoop(callItem)
-      } else {
-        setTimeout(() => {
-          startCallLoop(callItem)
-        }, interval - curTime + callItem.lastCall)
-      }
+      callItem.interval=interval
+      callItem.queue.updateInterval(interval)
+      callItem.args=args
+      // callItem.queue.updateDoneCb()
+      // callItem.valid = false
+      // try {
+      //   clearTimeout(callItem.timeoutId)
+      // } catch (e) {}
+      // let newCallItem = { cb, interval, args, valid: true }
+      // cacheCalls.splice(i, 1, newCallItem)
+      // let curTime = new Date().getTime()
+      // if (curTime - callItem.lastCall >= interval) {
+      //   startCallLoop(newCallItem)
+      // } else {
+      //   setTimeout(() => {
+      //     startCallLoop(newCallItem)
+      //   }, interval - curTime + callItem.lastCall)
+      // }
+      // startLoopAdd(callItem)
+      callItem.queue.addCall(cb)
       return
     }
   }
-  let callItem = { cb, interval, args }
+  // console.log('new invoke')
+  let queue=new IntervalQueue(interval,()=>{
+    console.log('setting tiemout',callItem.interval)
+    setTimeout(()=>{
+      callItem.queue.addCall(cb)
+    },callItem.interval)
+  })
+  let callItem = { cb, interval, args, valid: true, queue }
   cacheCalls.push(callItem)
-  startCallLoop(callItem)
+  // startCallLoop(callItem)
+  callItem.queue.addCall(cb)
+  // startLoopAdd(callItem)
 }
-function startCallLoop(callItem) {
-  let ret = callItem.cb(...callItem.args)
-  if (Object.prototype.toString.call(ret) === '[object Promise]') {
-    ret.catch(() => {}).then(() => {
-      callItem.lastCall = new Date().getTime()
-      callItem.timeoutId = setTimeout(() => {
-        startCallLoop(callItem)
-      }, callItem.interval)
-      console.log('new Timeout', callItem.timeoutId)
-    })
-  } else {
-    callItem.lastCall = new Date().getTime()
-    callItem.timeoutId = setTimeout(() => {
-      startCallLoop(callItem)
-    }, callItem.interval)
-    console.log('new Timeout', callItem.timeoutId)
+class IntervalQueue {
+  constructor(interval,doneCb){
+    this.queue=[]
+    this.interval=interval
+    this.lastCall=null
+    this.executing=false
+    this.scheduled=false
+    this.doneCb=doneCb
+  }
+  updateInterval(interval){
+    this.interval=interval
+  }
+  // updateDoneCb(){
+  //   this.doneCb=doneCb
+  // }
+  addCall(fn){
+    if(this.lastCall===null){
+      this.execute(fn,()=>{
+        this.lastCall=new Date().getTime()
+      })
+    }
+    else{
+      let curTime=new Date().getTime()
+      if(this.executing||this.scheduled){
+        this.queue.push(fn)
+      }
+      else if(curTime-this.lastCall<this.interval){
+        this.queue.push(fn)
+        // setTimeout()
+        this.scheduleCall(this.interval-curTime+this.lastCall)
+      }
+      else{
+        this.execute(fn,()=>{
+          this.lastCall=new Date().getTime()
+        })
+      }
+    }
+  }
+  scheduleCall(interval){
+    interval=interval||this.interval
+    if(this.queue.length>0){
+      setTimeout(()=>{
+        let fn=this.queue[this.queue.length-1]
+        this.execute(fn,()=>{
+          this.lastCall=new Date().getTime()
+        })
+        this.queue=[]
+      },interval)
+      this.scheduled=true
+    }
+  }
+  execute(fn,cb){
+    // debugger
+    this.executing=true
+    let ret=fn()
+    if(Object.prototype.toString.call(ret)==='[object Promise]'){
+      ret.catch(err=>{
+        console.log('get error',err)
+      }).then(()=>{
+        this.executing=false
+        this.scheduled=false
+        console.log('execute done')
+        cb&&cb()
+        this.doneCb&&this.doneCb()
+        this.scheduleCall()
+      })
+    }
+    else{
+      cb&&cb()
+      this.doneCb&&this.doneCb()
+    }
   }
 }
+let fn=()=>{return new Promise((resolve,reject)=>{setTimeout(()=>{console.log(new Date());resolve(1)},2000)})}
+intervalCall(fn, 1000)
+// export { intervalCall }

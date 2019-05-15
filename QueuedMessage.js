e2263b302e0e8687207c4a453db5be19887d6c84
yojhop <yojhop@126.com>
2019-05-15 16:24:27 +0800

    update queuedmessage

--- QueuedMessage.js
+++ QueuedMessage.js
@@ -1,58 +1,67 @@
 class QueuedMessage {
-    constructor({ handler, addFunc, timeout = 0 }) {
-      try {
-        this.addFunc = addFunc
-        this.queue = []
-        this.timeout = timeout
-        this.handler = handler
-        this.timeoutId = null
-        this.handleQueue = this.handleQueue.bind(this)
-        this.lastHandled = null
-        this.nextSet = null
-      } catch (err) {
-        console.log(err)
-      }
-    }
-    //   startProcess() {
-    //     this.queue = []
-    //     if (this.timeoutId) {
-    //       clearTimeout(this.timeoutId)
-    //     }
-    //     this.handleQueue()
-    //   }
-    handleQueue() {
-      // console.log('handle queue', this.queue)
-      if (this.queue.length > 0) {
-        this.lastHandled = this.getTimestamp()
-        const typeOf=Object.prototype.toString
-        if(typeOf.call(this.handler)==='[object Promise]'){
-          this.handler(this.queue)
-        }
-        this.handler(this.queue)
-        this.queue = []
-      }
-      // setTimeout(this.handleQueue, this.timeout)
-    }
-    getTimestamp() {
-      return new Date().getTime()
-    }
-    addMessage(msg) {
-      // console.log('adding message', msg)
-      this.addFunc ? this.addFunc(this.queue, msg) : this.queue.push(msg)
-      if (this.lastHandled === null) {
-        this.handleQueue()
-      } else {
-        if (this.lastHandled + this.timeout !== this.nextSet) {
-          setTimeout(this.handleQueue, this.timeout - this.getTimestamp() + this.lastHandled)
-          this.nextSet = this.lastHandled + this.timeout
-        }
-      }
-    }
-    updateTimeout(newTimeout) {
-      this.timeout = newTimeout
-    }
-    getTimeout() {
-      return this.timeout
+  constructor({ handler, addFunc, timeout = 0 }) {
+    try {
+      this.addFunc = addFunc
+      this.queue = []
+      this.timeout = timeout
+      this.handler = handler
+      this.timeoutId = null
+      this.handleQueue = this.handleQueue.bind(this)
+      this.lastHandled = null
+      this.nextSet = null
+      this.timeoutId = null
+    } catch (err) {
+      console.log(err)
     }
   }
-  
+  handleQueue() {
+    if (this.queue.length > 0) {
+      // console.log(new Date().getTime(), 'handled')
+      this.lastHandled = this.getTimestamp()
+      this.handler(this.queue)
+      this.queue = []
+    }
+  }
+  clear() {
+    this.queue = []
+  }
+  getTimestamp() {
+    return new Date().getTime()
+  }
+  addMessage(msg) {
+    this.addFunc ? this.addFunc(this.queue, msg) : this.queue.push(msg)
+    this.tryProcess()
+  }
+  addMessages(msgs) {
+    if (this.addFunc) {
+      for (let msg of msgs) this.addFunc(this.queue, msg)
+    } else this.queue = this.queue.concat(msgs)
+    this.tryProcess()
+  }
+  // 如果当前更新id不为null，则不做处理
+
+  // 如果当前更新id为空，如果当前时间与上次更新时间差大于timeout或者上次更新时间为Null，则直接处理队列，并且更新上次更新时间
+  // 如果更新id为空，并且当前时间与上次更新时间差小于timeout，则设置延迟处理队列，更新上次更新时间，处理完成后设置更新id为null,
+  // 用延迟返回设置更新id
+  tryProcess() {
+    if (this.timeoutId !== null) return
+    let curTs = this.getTimestamp()
+    if (this.lastHandled === null || curTs - this.lastHandled > this.timeout) {
+      this.handleQueue()
+    } else {
+      this.timeoutId = setTimeout(() => {
+        this.handleQueue()
+        this.timeoutId = null
+      }, this.timeout - (curTs - this.lastHandled))
+    }
+  }
+  updateTimeout(newTimeout) {
+    this.timeout = newTimeout
+  }
+  getTimeout() {
+    return this.timeout
+  }
+}
+
+export { QueuedMessage }
+
--- test.js
+++ test.js
@@ -1,7 +1,37 @@
-function test(cb){
-    
-}
-let p=new Promise((resolve,reject)=>{
-    setTimeout(()=>{resolve(1)},3000)
-})
-test(p)
+
+function formatDate(date){
+      var day = date.getDate(),
+          month = date.getMonth(),
+          year = date.getFullYear(),
+          hour = date.getHours(),
+          minute = date.getMinutes(),
+          second = date.getSeconds(),
+          miliseconds = date.getMilliseconds(),
+          h = hour % 12,
+          hh = twoDigitPad(h),
+          HH = twoDigitPad(hour),
+          mm = twoDigitPad(minute),
+          ss = twoDigitPad(second),
+          aaa = hour < 12 ? 'AM' : 'PM',
+          dd = twoDigitPad(day),
+          M = month + 1,
+          MM = twoDigitPad(M),
+          yyyy = year + "",
+          yy = yyyy.substr(2, 2)
+      ;
+      return 'yyyy-MM-ddTHH:mm:ss.S+08:00'
+        .replace('hh', hh).replace('h', h)
+        .replace('HH', HH).replace('H', hour)
+        .replace('mm', mm).replace('m', minute)
+        .replace('ss', ss).replace('s', second)
+        .replace('S', miliseconds)
+        .replace('dd', dd).replace('d', day).replace('MM', MM).replace('M', M)
+        .replace('yyyy', yyyy)
+        .replace('yy', yy)
+        .replace('aaa', aaa)
+      ;
+  }
+  function twoDigitPad(num) {
+      return num < 10 ? "0" + num : num;
+  }
+  console.log(formatDate(new Date(), 'yyyy-MM-ddTHH:mm:ss.S+08:00'));
